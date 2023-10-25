01e8bb0a4d2315c528abad1a8573f4e01f0dfa20
Pedro Enciso <penciso@ucsd.edu>
2023-10-25 13:21:09 -0700

    add router

--- src/App/index.jsx
+++ src/App/index.jsx
@@ -1,9 +1,31 @@
-import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import RootLayout from "../pages/Root";
+import HomePage from "../pages/Home";
+import AboutPage from "../pages/About";
+
 import "./Normalize.css";
 import "./BaseStyles.css";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout />,
+    children: [
+      {
+        index: true,
+        element: <HomePage />,
+      },
+      {
+        path: "about",
+        element: <AboutPage />,
+      },
+    ],
+  },
+]);
+
 function App(props) {
-  return <>App</>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
