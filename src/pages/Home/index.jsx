9ec7635a6435aef4e02c0d3bac8d40a79981d3a6
Pedro Enciso <penciso@ucsd.edu>
2023-10-27 13:18:34 -0700

    added ProjectSection & data

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 
 import HeroSection from "src/components/Hero";
+import ProjectsSection from "src/components/Projects";
+
+import data from "src/data.js";
 
 function HomePage(props) {
   return (
     <>
       <HeroSection />
+      <ProjectsSection projects={data} />
     </>
   );
 }
